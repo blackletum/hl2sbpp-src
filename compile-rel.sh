d67fa1fbe8ea60aa9e72b595e7f20f6a6bbe0b5e
ThePixelMoon <yourlocalmoon7@gmail.com>
2026-06-27 10:00:30 +0300

    No clang ig

--- compile-rel.sh
+++ compile-rel.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-CXX=clang++ CC=clang ./waf configure -T release --enable-opus --64bits --togles --prefix=../game --disable-warns
-CXX=clang++ CC=clang ./waf build install -p -vv -j$(nproc)
+./waf configure -T release --enable-opus --64bits --togles --prefix=../game --disable-warns
+./waf build install -p -vv -j$(nproc)
