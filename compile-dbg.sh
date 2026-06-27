280936f7ea00f93388831fbbaab15ed205371d3a
ThePixelMoon <yourlocalmoon7@gmail.com>
2026-06-27 09:59:30 +0300

    I blame ccache

--- compile-dbg.sh
+++ compile-dbg.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-CXX=clang++ CC=clang ./waf configure -T debug --use-ccache --enable-opus --64bits --togles --prefix=../game --disable-warns --sanitize=address,undefined
+CXX=clang++ CC=clang ./waf configure -T debug --enable-opus --64bits --togles --prefix=../game --disable-warns --sanitize=address,undefined
 CXX=clang++ CC=clang ./waf build install -p -vv -j$(nproc)
--- compile-rel.sh
+++ compile-rel.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
-CXX=clang++ CC=clang ./waf configure -T release --use-ccache --enable-opus --64bits --togles --prefix=../game --disable-warns
+CXX=clang++ CC=clang ./waf configure -T release --enable-opus --64bits --togles --prefix=../game --disable-warns
 CXX=clang++ CC=clang ./waf build install -p -vv -j$(nproc)
